d0a6d910d739b79aaefdae33fe1c37ca500cd247
JackHughes03 <101028724+JackHughes03@users.noreply.github.com>
2023-10-30 14:05:04 +0000

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -31,25 +31,25 @@
             <section class="mt-0">
                 <h2 id="intro">Introduction</h2>
                 
-                <p>Crypto seems to be an ever evolving currency which is becoming more and more popular. Although this may be good for factors including money growth and trust, just like anything, it comes with its downsides, specifically cyber issues...<br><br>
-                    In this infographic, I will visualise and discuss the current and potential crypto crimes and legal responses. Here is a diagram to visualise the percentage of how cyber attacks happen in crypto currency.
-                </p>
+                <p>Crypto seems to be an ever evolving currency which is becoming more and more popular. Although this may be good for factors including money growth and trust, just like anything, it comes with its downsides, specifically cyber issues...</p>
+                <p class="mt-6">In this infographic, I will visualise and discuss the current and potential crypto crimes and legal responses. Here is a diagram to visualise the percentage of how cyber attacks happen in crypto currency.</p>
             </section>
             
             <section>
                 <h2>Crypto Cyber Incidents</h2>
                 
                 <h3 id="pipeline">Colonial Pipeline ransomware</h3>
-                <p>Now due to the harder traceability of crypto currency transactions, a lot of hackers demand payment through some sort of crypto currency.<br><br>
-                    In may 2021, the Colonial Pipeline, a major fuel pipeline in the US, was hit by a ransomware attack that forced it to shutdown. The hackers demanded a payment of 4.4 million USD through Bitcoin in exchange for the decryption key to unlock the pipeline's systems.
-                </p>
+                <p>Now due to the harder traceability of crypto currency transactions, a lot of hackers demand payment through some sort of crypto currency.</p>
+                <p class="mt-6">In may 2021, the Colonial Pipeline, a major fuel pipeline in the US, was hit by a ransomware attack that forced it to shutdown. The hackers demanded a payment of 4.4 million USD through Bitcoin in exchange for the decryption key to unlock the pipeline's systems.</p>
+                <p class="mt-6">The pipeline was shut down for 6 days, resulting in a fuel shortage in the US. The pipeline's CEO, Joseph Blount, said that he authorised the payment because he was unsure how long the shutdown would last and did not want to risk the public's safety. Although the company authorised the payment, the FBI conducted an investigation into what happened, working closely with various cybersecurity experts and private partners to trace the attackers origin.</p>
+                <p class="mt-6">The FBI was able to recover some of the stolen funds.</p>
                 
-                <img src="../assets/images/pipeline.webp" alt="">
+                <img class="mt-10" src="../assets/images/pipeline.webp" alt="">
                 
                 <h3 id="polynetwork" class="mt-10">Poly network</h3>
-                <p>In August 2021, hackers stole cryptocurrencies worth over 600 million USD in the form of crypto from Poly Network, which is a finance platform. The hacker later on said they did it 'for fun', although significant damage was done to not only Poly Network's reputation, but their financial assets, resulting in money freezes.<br><br>
-                    The hacker returned the money, but the incident still raised questions about the security of crypto currency. Below is a visual representation of the hacker recieving the money.
-                </p>
+                <p>In August 2021, hackers stole cryptocurrencies worth over 600 million USD in the form of crypto from Poly Network, which is a finance platform. The hacker later on said they did it 'for fun', although significant damage was done to not only Poly Network's reputation, but their financial assets, resulting in money freezes.</p>
+                <p class="mt-6">The hacker returned the money, but the incident still raised questions about the security of crypto currency. Below is a visual representation of the hacker recieving the money.</p>
+                <p class="mt-6"></p>
                 
                 <img src="../assets/images/polynetwork.png" alt="">
             </section>
@@ -58,15 +58,22 @@
                 <h2 id="laws">Legislations and/or regulations</h2>
                 
                 <h3>UK</h3>
-                <p>Currently, the UK do not have any specific laws and regulations for crypto currency, however, they are looking at implementing some to regulate the growing crypto market.<br>
-                    Here are some recommendations:
-                </p>
+                <p>Currently, the UK do not have any specific laws and regulations for crypto currency, however, they are looking at implementing some to regulate the growing crypto market.</p>
+                <p class="mt-0">Here are some recommendations:</p>
                 
-                <ul class="text-white ml-20 mt-4 list-disc pr-10">
+                <ul class="list">
                     <li>Licenses and Registration for Crypto businesses - This will help the authorities to oversee what is going on more easier.</li>
                     <li>Anti-Money Laundering (AML) and Know Your Customer (KYC) - This will help to prevent money laundering and other illegal activities.</li>
                     <li>Consumer Protection - Crypto currency scams are huge right now and this needs to be regulated. This will help to protect consumers from said scams.</li>
                     <li>Market Integrity - The crypto market can be influenced by people with a big following, such as Elon Musk. Perhaps some laws regarding these activities need to be put in place.</li>
+                </ul>
+                
+                <h3 class="mt-10">USA</h3>
+                <p>The US have a few laws and regulations in place for crypto currency, however, they are not as strict as they could be. Here are some of the laws and regulations in place:</p>
+                
+                <ul class="list">
+                    <li>The Arizona State Senate introduced a bill that would amend the definition of legal tender to include Bitcoin.</li>
+                    <li>Iowa introduced a bill that would prohibit the state from accepting payment in the form of cryptocurrencies.</li>
                 </ul>
                 
                 <p class="mt-8">Below is a quick representational diagram map to showcase cryptocurrency regulations in each country.</p>
@@ -75,23 +82,7 @@
             </section>
             
             <section>
-                <h2>The Crime Unveiled</h2>
-            </section>
-            
-            <section>
-                <h2>The Culprits</h2>
-            </section>
-            
-            <section>
-                <h2>The Legal Response</h2>
-            </section>
-            
-            <section>
                 <h2>Global Response</h2>
-            </section>
-            
-            <section>
-                <h2>Protecting Your Assets</h2>
             </section>
             
             <section>
@@ -158,6 +149,10 @@
                 
                 img {
                     @apply w-[89%] max-w-4xl mt-4 mx-auto;
+                }
+                
+                .list {
+                    @apply text-white ml-20 mt-4 list-disc pr-10;
                 }
             }
         }
